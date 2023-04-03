ae6af236975baf9606d10132c7c39c3f600919ea
Your Name <you@example.com>
2023-04-03 11:22:32 -0600

    footer changes

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -6,9 +6,9 @@
 	class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mt-20 border-t border-white/20"
 >
 	<div class="text-white/70 flex items-center flex-none">
-		Powered by <span class="mx-1 font-bold"> OpenAI</span>
-		and
-		<span class="ml-1 font-bold">Vercel Edge Functions</span>.
+		Powered by <span class="mx-1 font-bold"> SamasatAI</span>
+		vía
+		<span class="ml-1 font-bold">Navi AI</span>.
 	</div>
 	<div class="flex items-center mt-2 md:mt-0">
 		<a class="group" aria-label="TaxPal on Twitter" href="https://twitter.com/steph_dietz_"
