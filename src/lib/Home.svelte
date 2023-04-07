c4bfa22d8d087e272cfdc2ff2665b8bd7325bae4
Your Name <you@example.com>
2023-04-07 08:35:44 -0600

    tercer boton

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,23 +1,18 @@
 <div class="w-full">
-	<div
-		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
-	>
-		Genera tu planeación <br class="hidden md:block" /> de clase
-		<br class="hidden md:block" /> con Navi AI
+	<div class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full">
+	  Genera tu planeación <br class="hidden md:block" /> de clase
+	  <br class="hidden md:block" /> con Navi AI
 	</div>
 	<div class="flex flex-wrap items-center mr-auto">
-		<button
-			on:click
-			class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8"
-		>
-			Generar Planeación de Clase
-		</button>
-		<a
-			href="https://www.navi-ia.com/"
-			target="_blank"
-			class="w-full md:w-auto text-center bg-white/20 hover:bg-white/50 mb-4 flex-none rounded-full text-white font-medium py-3 px-6"
-		>
-			Generador de Articulos
-		</a>
+	  <button on:click class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8">
+		Generar Planeación de Clase
+	  </button>
+	  <a href="https://www.navi-ia.com/" target="_blank" class="w-full md:w-auto text-center bg-white/20 hover:bg-white/50 mb-4 flex-none rounded-full text-white font-medium py-3 px-6">
+		Generador de Articulos
+	  </a>
+	  <a href="https://www.navi-ia.digital/" target="_blank" class="w-full md:w-auto text-center bg-gray-600 hover:bg-gray-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 ml-8">
+		Navi Tutor Socrático
+	  </a>
 	</div>
-</div>
+  </div>
+  
