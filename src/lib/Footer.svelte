cdb8b78ae4e820e4c176365fd118c9f85d2d358d
Your Name <you@example.com>
2023-05-23 07:31:56 -0600

    samsant leyenda

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -6,7 +6,7 @@
 	class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mt-20 border-t border-white/20"
 >
 	<div class="text-white/70 flex items-center flex-none">
-		Powered by <span class="mx-1 font-bold"> SamasatAI</span>
+		Created by Samasat Group for EMDI school Powered by <span class="mx-1 font-bold"> SamasatAI</span>
 		vía
 		<span class="ml-1 font-bold">Navi AI</span>.
 	</div>
@@ -21,3 +21,4 @@
 		<GitHubButton />
 	</div>
 </div>
+
