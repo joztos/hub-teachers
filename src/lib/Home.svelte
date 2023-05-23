280ac9aa8f7b7f5b5fa79b65c177c7a5ea25f1cc
Your Name <you@example.com>
2023-05-23 12:25:48 -0600

    Revert "Revert "main version and ux""
    This reverts commit ee6203fb332e7b85aafc71ff8de146b2b407c93e.

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,19 +1,16 @@
-<div class="w-full">
+<div class="w-full px-4 md:px-0">
 	<div class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full">
-	  Herramientas AI <br class="hidden md:block" /> para el profesor
+	  Herramientas AI <br class="hidden md:block" /> para estudiantes
 	  <br class="hidden md:block" /> con Navi AI
 	</div>
-	<div class="flex flex-wrap items-center mr-auto">
-	  <a href="https://demoai.samasat.info/" target="_blank" class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8">
-		Generador Planeación de Clase
-	  </a>
-	  <a href="https://genartnav.vercel.app/" target="_blank" class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6">
+	<div class="flex flex-wrap items-center justify-center md:justify-start">
+	  <a href="https://genartnav.vercel.app/" target="_blank" class="my-2 mx-2 md:mx-4 w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 rounded-full text-white font-medium py-3 px-6">
 		Generador de Articulos
 	  </a>
-	  <a href="https://demoai2.samasat.info/" target="_blank" class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 ml-8">
+	  <a href="https://demoai2.samasat.info/" target="_blank" class="my-2 mx-2 md:mx-4 w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 rounded-full text-white font-medium py-3 px-6">
 		Navi Tutor Socrático
 	  </a>
-	  <a href="https://demoai3.samasat.info/" target="_blank" class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6">
+	  <a href="https://demoai3.samasat.info/" target="_blank" class="my-2 mx-2 md:mx-4 w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 rounded-full text-white font-medium py-3 px-6">
 		Chatea con tu libro
 	  </a>
 	</div>
